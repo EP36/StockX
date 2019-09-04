9888b8fe8e8137cccdb6657228679f638fe1c92b
EP <ep@github.com>
2019-09-04 12:48:42 -0400

    typo fixed in server.js

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const { getData, getAvg, addData } = require('./db/pgController.js.js')
+const { getData, getAvg, addData } = require('./db/pgController.js')
 
 const app = express();
 
